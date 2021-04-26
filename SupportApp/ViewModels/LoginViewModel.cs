b56b285f10ec7da2810000642272a45372d0c4d7
Anton <doskimag2016@yandex.ru>
2021-04-26 21:53:06 +0300

    Login Control и начало мэйин виндоу( юзернэйм и сменить юзера)

---
+++ SupportApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Prism.Commands;
+using Prism.Mvvm;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SupportApp.ViewModels
+{
+    public class LoginViewModel : BindableBase
+    {
+        private string _login;
+        public event EventHandler<string> LoginSuccessfuly;
+        public string Login
+        {
+            get { return _login; }
+            set { SetProperty(ref _login, value); }
+        }
+
+        public ICommand SignInCommand { get; }
+
+        public ICommand SignUpCommand { get; }
+
+        public LoginViewModel()
+        {
+            SignInCommand = new DelegateCommand(SignIn, () => !string.IsNullOrWhiteSpace(_login)).ObservesProperty(() => Login);
+            SignUpCommand = new DelegateCommand(SignUp, () => !string.IsNullOrWhiteSpace(_login)).ObservesProperty(() => Login);
+
+        }
+
+        private void SignIn()
+        {
+            OnLoginSuccessfuly(Login);
+            Login = string.Empty;
+        }
+
+        private void SignUp()
+        {
+            OnLoginSuccessfuly(Login);
+            Login = string.Empty;
+        }
+
+        protected virtual void OnLoginSuccessfuly(string login)
+        {
+            LoginSuccessfuly?.Invoke(this, login);
+        }
+    }
+}
--- SupportApp/ViewModels/MainWindowViewModel.cs
+++ SupportApp/ViewModels/MainWindowViewModel.cs
@@ -3,11 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Prism.Commands;
 using Prism.Mvvm;
 
 namespace SupportApp.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
+        private bool _isOpenLogin = true;
+        private string _loginName;
+        public LoginViewModel Login { get; } = new LoginViewModel();
+
+        public bool IsOpenLogin
+        {
+            get { return _isOpenLogin; }
+            set { SetProperty(ref _isOpenLogin, value); }
+        }
+
+        public string LoginName
+        {
+            get { return _loginName; }
+            set { SetProperty(ref _loginName, value); }
+        }
+        public ICommand LogOutCommand { get; }
+        public MainWindowViewModel()
+        {
+          Login.LoginSuccessfuly += LoginOnLoginSuccessfuly;  
+          LogOutCommand = new DelegateCommand(LogOut, () => !IsOpenLogin).ObservesProperty(() => IsOpenLogin);
+        }
+
+        private void LogOut()
+        {
+            LoginName = string.Empty;
+            IsOpenLogin = true;
+        }
+
+        private void LoginOnLoginSuccessfuly(object sender, string loginName)
+        {
+            IsOpenLogin = false;
+            LoginName = loginName;
+        }
     }
 }
